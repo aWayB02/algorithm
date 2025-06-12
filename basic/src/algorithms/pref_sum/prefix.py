78490f471501a902dce49145fc1bef8f2183f8a3
BadGateway5 <khabibtime@rambler.ru>
2025-06-12 21:54:49 +0300

    update

--- basic/src/algorithms/pref_sum/prefix.py
+++ basic/src/algorithms/pref_sum/prefix.py
@@ -4,11 +4,14 @@ class PrefixSum:
     и создает префиксный массив на его основе.
     """
 
-    def __init__(self, nums: list, one_based=False):
+    def __init__(self, nums: list):
 
         self.prefix_arr = [0]
         for num in nums:
             self.prefix_arr.append(self.prefix_arr[-1] + num)
 
     def sum(self, l, r):
+        """
+        Функция для вычисления суммы на отрезке
+        """
         return self.prefix_arr[r] - self.prefix_arr[l - 1]
--- basic/tests/test_prefix_sum.py
+++ basic/tests/test_prefix_sum.py
@@ -2,17 +2,25 @@ import pytest
 from src.algorithms.pref_sum import PrefixSum
 
 
+@pytest.fixture()
+def getPrefix():
+    nums = [10, 20, 30, 40, 50]
+    return PrefixSum(nums)
+
+
 class TestPrefixSum:
 
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.nums = [10, 20, 30]
-        self.prefix = PrefixSum(self.nums)
-
-    @pytest.mark.parametrize("l, r, expected", [(1, 3, 60), (1, 2, 30), (2, 3, 50)])
-    def test_sum(self, l, r, expected):
-
-        assert self.prefix.sum(l, r) == expected
-
-
-# 0 10 30 60
+    @pytest.mark.parametrize(
+        "l, r, expected",
+        [
+            (1, 3, 60),
+            (1, 2, 30),
+            (2, 3, 50),
+            (1, 1, 10),
+            (1, 4, 100),
+            (4, 5, 90),
+            (3, 5, 120),
+        ],
+    )
+    def testSum(self, getPrefix: PrefixSum, l, r, expected):
+        assert getPrefix.sum(l, r) == expected
